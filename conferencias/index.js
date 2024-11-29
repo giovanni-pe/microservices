463f14f134bb50a203c8db8f69dab96d5f83367b
giovanni <gkpe24@gmail.com>
2024-11-29 12:59:20 -0500

    add conferece dependencies ip

--- conferencias/index.js
+++ conferencias/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(cors());
 
 // Replace with your actual target endpoint
-const targetEndpoint = 'http://3.128.30.195';
+const targetEndpoint = 'http://52.15.89.201';
 //change beta 
 app.get('/', async (req, res) => {
     let targetResponseTime = 0;
